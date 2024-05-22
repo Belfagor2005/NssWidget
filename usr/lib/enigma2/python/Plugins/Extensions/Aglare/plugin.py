d7d055e0d12ccf1866f9deaa6f2f5bd3e739de19
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-05-22 15:38:50 +0200

    Summary upload: new screen Meteo

--- usr/lib/enigma2/python/Plugins/Extensions/Aglare/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/plugin.py
@@ -54,8 +54,9 @@ cfg.FontStyle = ConfigSelection(default='basic', choices=[
  ('font3', _('BebasNeue'))])
 cfg.skinSelector = ConfigSelection(default='base', choices=[
  ('base', _('Default'))])
-cfg.InfobarStyle = ConfigSelection(default='infobar_no_posters', choices=[
+config.plugins.AglareNss.InfobarStyle = ConfigSelection(default='infobar_no_posters', choices=[
  ('infobar_no_posters', _('Infobar_NO_Posters')),
+ ('infobar_posters_meteo', _('Infobar_Posters_Meteo')),
  ('infobar_posters', _('Infobar_Posters'))])
 cfg.SecondInfobarStyle = ConfigSelection(default='secondinfobar_no_posters', choices=[
  ('secondinfobar_no_posters', _('SecondInfobar_NO_Posters')),
