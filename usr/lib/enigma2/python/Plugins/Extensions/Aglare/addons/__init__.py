46a81097f02b56c2d108213a8536233456d04bdf
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-13 20:39:19 +0100

    Add files via upload

---
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ======================================================================
+# LinuxsatPanel Plugin
+# Coded by masterG - oktus - pcd
+#
+# rewritten by Lululla at 20240720
+#
+# ATTENTION PLEASE...
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2, or (at your option) any later
+# version.
+# You must not remove the credits at
+# all and you must make the modified
+# code open to everyone. by Lululla
+# ======================================================================
+
+
+from __future__ import absolute_import
+__author__ = "Lululla"
+__email__ = "ekekaz@gmail.com"
+__copyright__ = 'Copyright (c) 2024 Lululla'
+__license__ = "GPL-v2"
+__version__ = "1.0.0"
