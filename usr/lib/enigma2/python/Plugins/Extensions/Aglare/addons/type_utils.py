a20b3dff4442735d08766bc0bdaa1401ac11ed0d
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-03-08 16:10:41 +0100

    -

--- usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/type_utils.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/type_utils.py
@@ -1,57 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#  This plugin is NOT free software. It is open source, you are allowed to
-#  modify it (if you keep the license), but it may not be commercially
-#  distributed other than under the conditions noted above.
-#  Lululla coder 2022.07.20
-# NOT REMOVE DISCLAIMER!!!
-
-from Components.ActionMap import ActionMap
-from Components.Label import Label
-from Components.MenuList import MenuList
-# from Screens.HelpMenu import HelpableScreen
-from Screens.Screen import Screen
-from Tools.Directories import fileExists
-from enigma import eLabel
-from errno import ENOENT
 from os import system
+# Components
+from Components.config import config
+from Components.Label import Label
+from Components.ActionMap import ActionMap
+from Components.MenuList import MenuList
+from enigma import eLabel
 
+# Screens
+from Screens.Screen import Screen
+from Screens.MessageBox import MessageBox
+from Screens.HelpMenu import HelpableScreen
 
+# Tools
+from Tools.Directories import fileExists  # , fileReadLines
+from errno import ENOENT
+
+from sys import _getframe as getframe
 DEFAULT_MODULE_NAME = __name__.split(".")[-1]
+
+# Various
+import skin
+# by lululla
+
+
 pname = _("File Commander - Addon")
 pdesc = _("play/show Files")
-pversion = "1.0-r1"
-
-
-'''
-# def answercheck(self, answer=None):
-    # if answer is None:
-        # self.session.openWithCallback(self.answercheck, MessageBox, _("This operation checks if the skin has its components (is not sure)..\nDo you really want to continue?"))
-    # else:
-        # zaddons = os.path.join(thisdir, 'addons')
-        # if os.path.exists(zaddons):
-            # from .addons import checkskin
-            # self.check_module = eTimer()
-            # check = checkskin.check_module_skin()
-            # try:
-                # self.check_module_conn = self.check_module.timeout.connect(check)
-            # except:
-                # self.check_module.callback.append(check)
-            # self.check_module.start(100, True)
-            # self.openVi()
-
-# def openVi(self):
-    # from .addons.type_utils import File_Commander
-    # user_log = '/tmp/my_debug.log'
-    # if fileExists(user_log):
-        # self.session.open(File_Commander, user_log)
-'''
-
-
-# Calls onto the static function in eLabel. This avoids causing an invalidate
-# on the parent container which is detrimental to UI performance,
-# particularly in a complex screen like the graph EPG
+pversion = "1.0-r3"
 
 
 def getTextBoundarySize(instance, font, targetSize, text):
@@ -73,17 +50,23 @@ def fileReadLines(filename, default=None, source=DEFAULT_MODULE_NAME, debug=Fals
 class File_Commander(Screen):
 
     skin = """
-        <screen name="File_Commander" position="40,80" size="1900,900" title="Linuxsat-support - File Commander">
-            <widget name="list_head" position="8,10" size="1850,45" font="Regular;20" foregroundColor="#00fff000" />
-            <widget name="filedata" scrollbarMode="showOnDemand" position="10,60" size="1850,750" itemHeight="50" />
-            <widget name="key_red" position="100,840" size="260,25" transparent="1" font="Regular;20" />
-            <widget name="key_green" position="395,840" size="260,25" transparent="1" font="Regular;20" />
-            <widget name="key_yellow" position="690,840" size="260,25" transparent="1" font="Regular;20" />
-            <widget name="key_blue" position="985,840" size="260,25" transparent="1" font="Regular;20" />
-            <ePixmap position="70,840" size="260,25" zPosition="0" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
-            <ePixmap position="365,840" size="260,25" zPosition="0" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
-            <ePixmap position="660,840" size="260,25" zPosition="0" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
-            <ePixmap position="955,840" size="260,25" zPosition="0" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
+        <screen name="File_Commander" position="40,80" size="1900,900" title="Lululla_Commander">
+            <widget name="list_head" position="8,10" size="1850,45" font="Regular;24" foregroundColor="#00fff000" />
+            <widget name="filedata" scrollbarMode="showOnDemand" itemHeight="45" position="9,78" size="1850,725" />
+            <!--
+            <widget name="key_red" position="100,840" size="260,40" transparent="1" font="Regular;24" />
+            <widget name="key_green" position="395,840" size="260,40" transparent="1" font="Regular;24" />
+            <widget name="key_yellow" position="690,840" size="260,40" transparent="1" font="Regular;24" />
+            <widget name="key_blue" position="985,840" size="260,40" transparent="1" font="Regular;24" />
+            -->
+            <widget name="key_red" position="95,820" zPosition="19" size="260,40" transparent="1" font="Regular;24" halign="center" />
+            <widget name="key_green" position="395,820" zPosition="19" size="260,40" transparent="1" font="Regular;24" halign="center" />
+            <widget name="key_yellow" position="690,820" zPosition="19" size="260,40" transparent="1" font="Regular;24" halign="center" />
+            <widget name="key_blue" position="985,820" zPosition="19" size="260,40" transparent="1" font="Regular;24" halign="center" />
+            <ePixmap position="95,865" size="260,25" zPosition="0" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
+            <ePixmap position="395,865" size="260,25" zPosition="0" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
+            <ePixmap position="690,865" size="260,25" zPosition="0" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
+            <ePixmap position="985,870" size="260,25" zPosition="0" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
         </screen>"""
 
     def __init__(self, session, file):
@@ -91,38 +74,32 @@ class File_Commander(Screen):
         Screen.__init__(self, session)
         # HelpableScreen.__init__(self)
         self.file_name = file
-        title = "Linuxsat-support - File Commander"
+        title = "Lululla File Commander"
+        self.newtitle = title == 'vEditorScreen' and ('Console') or title
         self.list = []
         self["filedata"] = MenuList(self.list)
         self["actions"] = ActionMap(["WizardActions", "ColorActions", "DirectionActions"], {
-            "ok": self.exitEditor,
-            "green": self.exitEditor,
+            "ok": self.edit_Line,
+            "green": self.SaveFile,
             "back": self.exitEditor,
             "red": self.exitEditor,
-            "yellow": self.SaveFile,
-            "blue": self.exitEditor,
+            "yellow": self.del_Line,
+            "blue": self.ins_Line,
             # "chplus": self.posStart,
             # "chminus": self.posEnd,
         }, -1)
         self["list_head"] = Label(self.file_name)
         self["key_red"] = Label(_("Exit"))
-        self["key_green"] = Label(_("Exit"))
-        self["key_yellow"] = Label(_("Save"))
-        self["key_blue"] = Label(_("Exit"))
+        self["key_green"] = Label(_("Save"))
+        self["key_yellow"] = Label(_("Del Line"))
+        self["key_blue"] = Label(_("Ins Line"))
         self.selLine = None
         self.oldLine = None
         self.isChanged = False
-        self.skinName = "vEditorScreen"
         self.GetFileData(file)
-        self.newtitle = title == 'vEditorScreen' and ('Console') or title
-        self.onShown.append(self.updateTitle)
-
-    def updateTitle(self):
         self.setTitle(self.newtitle)
 
     def exitEditor(self):
-        # if fileExists(self.file_name):
-            # remove(self.file_name)
         self.close()
 
     def GetFileData(self, fx):
@@ -130,7 +107,7 @@ class File_Commander(Screen):
         if lines:
             for idx, line in enumerate(lines):
                 self.list.append(str(idx + 1).zfill(4) + ": " + line)
-        self["list_head"] = Label(fx)
+        self["list_head"].setText(fx)
 
     def posStart(self):
         self.selLine = 0
@@ -139,6 +116,20 @@ class File_Commander(Screen):
     def posEnd(self):
         self.selLine = len(self.list)
         self["filedata"].moveToIndex(len(self.list) - 1)
+
+    def edit_Line(self):
+        self.selLine = self["filedata"].getSelectionIndex()
+        if self.selLine is not None and 0 <= self.selLine < len(self.list):
+            current_line = self.list[self.selLine][0]
+            from Screens.VirtualKeyBoard import VirtualKeyBoard
+            self.session.openWithCallback(self.VirtualKeyBoardCallback, VirtualKeyBoard, title="Edit Line", text=current_line)
+
+    def VirtualKeyBoardCallback(self, callback=None):
+        if callback is not None and len(callback):
+            new_line = callback
+            self.list[self.selLine] = (new_line,)  
+            self.isChanged = True
+            self.refreshList()  # Rendi visibile la lista aggiornata
 
     def del_Line(self):
         self.selLine = self["filedata"].getSelectionIndex()
@@ -162,18 +153,19 @@ class File_Commander(Screen):
             lineno += 1
         self["filedata"].setList(self.list)
 
-    def SaveFile(self, answer):
-        if answer is True:
-            try:
-                if fileExists(self.file_name):
-                    system("cp " + self.file_name + " " + self.file_name + ".bak")
-                eFile = open(self.file_name, "w")
+    def SaveFile(self):
+        try:
+            if fileExists(self.file_name):
+                import shutil
+                shutil.copy(self.file_name, self.file_name + ".bak")
+
+            with open(self.file_name, "w") as eFile:
                 for x in self.list:
+                    if isinstance(x, tuple):
+                        x = x[0]
                     my_x = x.partition(": ")[2]
-                    eFile.writelines(my_x)
-                eFile.close()
-            except OSError:
-                pass
-            self.close()
-        else:
-            self.close()
+                    eFile.write(my_x + "\n")
+
+        except (OSError, IOError) as e:
+            print("Error saving file:", str(e))
+        self.close()
